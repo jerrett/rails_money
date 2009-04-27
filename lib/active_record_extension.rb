fe71aa49439ce64b43c33b9bf79abe77feeb9acf
Kristopher Stice <kristopher.stice@digitalmaelstrom.net>
2009-04-27 18:15:25 -0500

    * With these changes database column names do not need to be changed * Works through Associations & regular object pulls from the find methods * Will require each model that has Money Fields to update the model with  (Example:   money_format :rate_in_cents, :expense_in_cents) * Now a developer can see the public methods  for the "field"_in_cents on the model when typing model.public_methods  (Should show "field" and "field=")

--- lib/active_record_extension.rb
+++ lib/active_record_extension.rb
@@ -1,25 +1,14 @@
-module RailsMoney
-  def method_missing( method_id, *args )
-    method_name = method_id.to_s
-    setter = method_name.chomp!("=")
-    method_name = "#{method_name}_in_cents"
-
-    if @attributes.include?(method_name)
-      if setter
-        money = args.first.kind_of?(Money) ? args.first : Money.new(args.first)
-        write_attribute(method_name,money.cents) 
-      else
-        Money.create_from_cents(read_attribute(method_name))      
+class ActiveRecord::Base
+  def self.money_format(*fields)
+    fields.each do |field|
+      temp_field = field.to_s.sub(/(_in_cents)/,"")
+      define_method("#{temp_field}=") do |value|
+        money = value.kind_of?(Money) ? value : Money.new(value)
+        return self.send("#{temp_field}_in_cents=",money.cents)
       end
-    else 
-      super
+      define_method("#{temp_field}") do
+        return Money.create_from_cents(self.send("#{temp_field}_in_cents")) 
+      end
     end
   end
- 
-  def respond_to?( method, include_private = false )
-    method_name = method.to_s.chomp("=")
-    @attributes.include?("#{method_name}_in_cents") || super
-  end
 end
-
-ActiveRecord::Base.send :include, RailsMoney
